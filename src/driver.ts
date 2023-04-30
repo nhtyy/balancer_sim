a31443ac3a38c25c42f51ca25ddd7b0190d683f6
N <mail@nuhhtyy.xyz>
2023-04-30 13:28:10 -0400

    more logs

--- src/driver.ts
+++ src/driver.ts
@@ -1,11 +1,9 @@
 import { State } from "./state";
 
-const btc_usd = 29_000;
-const eth_usd = 1900;
-
 export function driver(state: State, hooks: ((state: State) => void)[]) {
   let duration = state.pool.rebalance.duration;
   let tokens = state.pool.tokens;
+
   const before_balance_0 = state.pool.balances.get(tokens[0]);
   const before_balance_1 = state.pool.balances.get(tokens[1]);
 
@@ -15,11 +13,6 @@ export function driver(state: State, hooks: ((state: State) => void)[]) {
 
     // hooks can mutate state
     hooks.forEach((hook) => hook(state));
-
-    // console.log({ height });
-    // console.log({
-    //   spot_price: state.pool.spot_price(height, tokens[0], tokens[1]),
-    // });
 
     state.advance();
   }
@@ -34,7 +27,9 @@ export function driver(state: State, hooks: ((state: State) => void)[]) {
     before_balance_1,
     after_balance_0,
     after_balance_1,
-    diff_0: (after_balance_0! - before_balance_0!) * eth_usd,
-    diff_1: (after_balance_1! - before_balance_1!) * btc_usd,
+    diff_0:
+      (after_balance_0! - before_balance_0!) * state.prices_usd.get(tokens[0])!,
+    diff_1:
+      (after_balance_1! - before_balance_1!) * state.prices_usd.get(tokens[1])!,
   });
 }
